c930e642700bc904ff6ea214062ce44c755a0a95
Shreya Pimprikar <shreya.pimprikar@ibm.com>
2023-12-31 19:52:18 +0530

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM openjdk: 17
+EXPOSE 8080
+ADD target/java-cicd.jar java-cicd.jar
+ENTRYPOINT ["java","-jar","/java-cicd.jar"]
--- src/main/java/com/cicd/githubactions/GithubCicdActionsApplication.java
+++ src/main/java/com/cicd/githubactions/GithubCicdActionsApplication.java
@@ -2,9 +2,17 @@ package com.cicd.githubactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
+	
+	@GetMapping("/welcome")
+	public String welcome() {
+		return "welcome to java world";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
